694b344bacd06c4cfb373f21748fb94f1188fae6
Chema <josemaria.cordero@ada-animaldata.com>
2026-04-29 11:31:59 +0200

    main

--- my-dapp/src/main.tsx
+++ my-dapp/src/main.tsx
@@ -1,16 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { WagmiProvider, createConfig, http } from 'wagmi'
-//import { sepolia } from 'wagmi/chains'
 import { polygon } from 'wagmi/chains'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-
 
 const config = createConfig({
   chains: [polygon],
   transports: {
-    [polygon.id]: http(),
+    [polygon.id]: http('https://polygon-bor-rpc.publicnode.com'),
   },
 })
 
